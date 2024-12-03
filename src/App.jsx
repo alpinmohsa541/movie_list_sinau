e8e3f266774dc312fd811a20a2411684850e11be
alpinmohsa541 <lpinmohsa@gmail.com>
2024-12-03 15:16:16 +0700

    Feat : inisiasi vite react dan tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS with React!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';  // Impor file CSS yang sudah diatur dengan Tailwind
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
